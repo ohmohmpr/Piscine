fba7a35bca1539e94cd4f29b727b468a01548556
Panyawat Rattana <nanoic_ohm@hotmail.com>
2026-01-28 20:58:52 +0100

    C03, ex02, 1 attempt

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,27 @@
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	len_dest;
+
+	i = 0;
+	len_dest = ft_strlen(dest);
+	while (src[i])
+	{
+		dest[len_dest + i] = src[i];
+		i++;
+	}
+	dest[len_dest + i] = '\0';
+	return (dest);
+}
