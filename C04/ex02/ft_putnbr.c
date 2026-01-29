a7d1ba25e54a65ada13a45f84912826139bf0e56
Panyawat Rattana <nanoic_ohm@hotmail.com>
2026-01-29 10:58:33 +0100

    C04, ex02, 1 attempt

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	char	remainder;
+	int		quotient;
+	int		base;
+
+	base = 10;
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(-1 * (nb % 10));
+		return ;
+	}
+	if (nb < 0)
+	{
+		nb = -1 * nb;
+		write(1, "-", 1);
+	}
+	quotient = nb / base;
+	remainder = nb % base + '0';
+	if (quotient != 0)
+	{
+		ft_putnbr(quotient);
+	}
+	write(1, &remainder, 1);
+}
