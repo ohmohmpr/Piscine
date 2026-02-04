930d9d2e99b7a842f0216bf23aa80af0f2179154
Panyawat Rattana <nanoic_ohm@hotmail.com>
2026-02-04 23:03:50 +0100

    edit

--- C04/ex00/ft_strlen.c
+++ C04/ex00/ft_strlen.c
@@ -1,12 +1,19 @@
+//#include <stdio.h>
+
 int	ft_strlen(char *str)
 {
 	int	c;
 
 	c = 0;
-	while (*str)
+	while (str[c])
 	{
-		str++;
 		c++;
 	}
 	return (c);
 }
+/*
+int	main(void)
+{
+	printf("len: %d \n", ft_strlen("0123456789"));
+	return (0);
+}*/
