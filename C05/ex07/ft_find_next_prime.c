87fa2aa29b61fbf616c14f460bb71e3e158f996f
Panyawat Rattana <nanoic_ohm@hotmail.com>
2026-02-10 21:01:06 +0100

    ex07, 08

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= nb / 2)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb <= 2)
+		return (2);
+	while (ft_is_prime(nb) == 0)
+		nb++;
+	return (nb);
+}
+/*
+int	main(void)
+{
+	printf("%d\n", ft_find_next_prime(100));
+	return (0);
+}*/
